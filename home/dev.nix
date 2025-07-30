eef671f5f9811459c2ae6589d26ac79fbaf0cced
Brandon Bennett <bbennett@coreweave.com>
2025-07-30 13:39:32 -0600

    home: Add shell and git home modules

--- home/dev.nix
+++ home/dev.nix
@@ -9,6 +9,10 @@
   };
 
   config = lib.mkIf config.bbennett.dev.enable {
+    home.sessionPath = [
+      "$HOME/.local/go/bin"
+    ];
+
     home.packages = with pkgs; [
       ast-grep
       delta
@@ -61,6 +65,12 @@
       rustup
       lldb
     ];
+
+    programs.go = {
+      enable = true;
+      goPath = ".local/go";
+      telemetry.mode = "off";
+    };
 
     programs.lazygit = {
       enable = true;
