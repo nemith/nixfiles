8886f8db6254d714ae45ed7cd7f55da5359154e7
Brandon Bennett <bbennett@coreweave.com>
2025-08-11 14:33:40 -0600

    hosts: fix missing matchBlocks for ssh config for cw-laptop

--- hosts/cw-laptop/home.nix
+++ hosts/cw-laptop/home.nix
@@ -32,31 +32,33 @@ in {
   };
 
   programs.ssh = {
-    "10.* *.cwint.ai" = {
-      forwardAgent = true;
-      proxyJump = "vdi";
-    };
+    matchBlocks = {
+      "10.* *.cwint.ai" = {
+        forwardAgent = true;
+        proxyJump = "vdi";
+      };
 
-    "metal-ztp*" = {
-      user = "acc";
-    };
-    "vdi" = {
-      hostname = vdiHostname;
-      forwardAgent = true;
-    };
-    "dev" = {
-      hostname = vdiHostname;
-      forwardAgent = true;
-      remoteForwards = lib.optionals config.bbennett.lemonade.server.enable [
-        {
-          bind.port = 2489;
-          host.address = "localhost";
-          host.port = 2489;
-        }
-      ];
-      extraOptions = {
-        RequestTTY = "yes";
-        RemoteCommand = "zellij attach --create dev";
+      "metal-ztp*" = {
+        user = "acc";
+      };
+      "vdi" = {
+        hostname = vdiHostname;
+        forwardAgent = true;
+      };
+      "dev" = {
+        hostname = vdiHostname;
+        forwardAgent = true;
+        remoteForwards = lib.optionals config.bbennett.lemonade.server.enable [
+          {
+            bind.port = 2489;
+            host.address = "localhost";
+            host.port = 2489;
+          }
+        ];
+        extraOptions = {
+          RequestTTY = "yes";
+          RemoteCommand = "zellij attach --create dev";
+        };
       };
     };
   };
