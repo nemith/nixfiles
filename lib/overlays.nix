17bade68cc50695fde8f445ab65b38a54adc6a10
Brandon Bennett <bbennett@coreweave.com>
2025-09-25 09:16:07 -0600

    feat: Use startship to generate prompt

--- lib/overlays.nix
+++ lib/overlays.nix
@@ -1,6 +1,21 @@
 _: {
-  myPackages = _: prev: {
+  myPackages = final: prev: {
     litra-autotoggle = prev.callPackage ../pkgs/litra-autotoggle.nix {};
-    # Add more custom packages here as they grow
+    starship-jj = prev.callPackage ../pkgs/starship-jj.nix {};
+
+    # Build from master until 0.24.0 is released
+    starship = prev.starship.overrideAttrs (_: rec {
+      version = "0.24.0-prerelease1";
+      src = prev.fetchFromGitHub {
+        owner = "starship";
+        repo = "starship";
+        rev = "2c11c086b8da5ea43ae795fa0e71a621e983ebcc";
+        hash = "sha256-+1PkDpHO+Ae52O1Reb+dcnmJ2g8HdriMgOQYLW4RY8A=";
+      };
+      cargoDeps = final.rustPlatform.fetchCargoVendor {
+        inherit src;
+        hash = "sha256-ltffMfuTt6v4Kzr5LCbMs2XyIQVhRSwItM1KSlSMyjY=";
+      };
+    });
   };
 }
