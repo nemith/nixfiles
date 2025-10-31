0cf101426cf5c17904dbb20fe65fd554fae9e415
Brandon Bennett <bbennett@coreweave.com>
2025-10-31 09:07:34 -0600

    remove unneeded overlays

--- lib/overlays.nix
+++ lib/overlays.nix
@@ -1,4 +1,4 @@
-final: prev: {
+_: prev: {
   litra-autotoggle = prev.callPackage ../pkgs/litra-autotoggle.nix {};
   starship-jj = prev.callPackage ../pkgs/starship-jj.nix {};
 
@@ -27,35 +27,4 @@ final: prev: {
         }
       )
     ];
-
-  # Fails on darwin
-  sbcl = prev.sbcl.overrideAttrs (_: {
-    doCheck = false;
-  });
-
-  # Disable failing tests until fixed upstream
-  # See: https://github.com/NixOS/nixpkgs/pull/450487
-  trurl = prev.trurl.overrideAttrs (_: {
-    doCheck = false;
-  });
-
-  # No fucking clue why this one fails tests
-  tealdeer = prev.tealdeer.overrideAttrs (_: {
-    doCheck = false;
-  });
-
-  # Build from master until 0.24.0 is released
-  starship = prev.starship.overrideAttrs (_: rec {
-    version = "0.24.0-prerelease1";
-    src = prev.fetchFromGitHub {
-      owner = "starship";
-      repo = "starship";
-      rev = "2c11c086b8da5ea43ae795fa0e71a621e983ebcc";
-      hash = "sha256-+1PkDpHO+Ae52O1Reb+dcnmJ2g8HdriMgOQYLW4RY8A=";
-    };
-    cargoDeps = final.rustPlatform.fetchCargoVendor {
-      inherit src;
-      hash = "sha256-ltffMfuTt6v4Kzr5LCbMs2XyIQVhRSwItM1KSlSMyjY=";
-    };
-  });
 }
