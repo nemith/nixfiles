9b096b4583c53a50fdea2a49497cd5c8a9373946
Brandon Bennett <bbennett@coreweave.com>
2026-02-13 12:24:58 -0700

    Fix python3Packages.rapidfuzz on darwin

--- lib/overlays.nix
+++ lib/overlays.nix
@@ -1,6 +1,21 @@
-_: prev: {
+final: prev: {
   litra-autotoggle = prev.callPackage ../pkgs/litra-autotoggle {};
   starship-jj = prev.callPackage ../pkgs/starship-jj {};
+
+  pythonPackagesExtensions =
+    prev.pythonPackagesExtensions
+    ++ [
+      (_: python-prev: {
+        # https://github.com/NixOS/nixpkgs/pull/490176
+        rapidfuzz = python-prev.rapidfuzz.overridePythonAttrs (old: {
+          nativeBuildInputs =
+            (old.nativeBuildInputs or [])
+            ++ final.lib.optionals final.stdenv.cc.isClang [
+              final.clang-tools
+            ];
+        });
+      })
+    ];
 
   # Click API changed for version 8.2, causing test failures
   cloudsmith-cli = prev.cloudsmith-cli.overrideAttrs (oldAttrs: {
