dd255334c6ca0a0248fe6cb72c439e61aab6be70
Brandon Bennett <bbennett@coreweave.com>
2026-03-31 10:19:44 -0600

    remove gdb overlay

--- lib/overlays.nix
+++ lib/overlays.nix
@@ -2,21 +2,6 @@ _: prev: {
   litra-autotoggle = prev.callPackage ../pkgs/litra-autotoggle {};
   starship-jj = prev.callPackage ../pkgs/starship-jj {};
 
-  # https://github.com/NixOS/nixpkgs/pull/486335/
-  gdb =
-    if prev.stdenv.isDarwin
-    then
-      prev.gdb.overrideAttrs (oldAttrs: {
-        configureFlags =
-          map
-          (flag:
-            if flag == "--enable-werror"
-            then "--disable-werror"
-            else flag)
-          oldAttrs.configureFlags;
-      })
-    else prev.gdb;
-
   cloudsmith-cli = prev.cloudsmith-cli.overrideAttrs (oldAttrs: {
     postPatch =
       (oldAttrs.postPatch or "")
