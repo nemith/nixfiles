bc13a853d3f533e5051be7be3ae40e4f2248461b
Brandon Bennett <brandon@brbe.me>
2026-05-27 18:07:23 -0600

    feat: package cockroach-sql

--- modules/roles/devel.nix
+++ modules/roles/devel.nix
@@ -4,6 +4,7 @@
     { pkgs, ... }:
     {
       imports = [
+        self.modules.homeManager.cockroachdb
         self.modules.homeManager.git
         self.modules.homeManager.go
         self.modules.homeManager.jujutsu
