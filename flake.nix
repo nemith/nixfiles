dd214aaebdec8676a846eb80ab42724b028072fc
Brandon Bennett <brandon@brbe.me>
2026-05-27 18:07:23 -0600

    feat: add nixpkgs module with overlay support

--- flake.nix
+++ flake.nix
@@ -57,14 +57,5 @@
         "x86_64-linux"
         "aarch64-linux"
       ];
-
-      perSystem =
-        { system, ... }:
-        {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-          };
-        };
     };
 }
---
+++ modules/nixpkgs.nix
@@ -0,0 +1,17 @@
+{ self, inputs, config, ... }:
+{
+  perSystem =
+    { system, ... }:
+    {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = builtins.attrValues config.flake.overlays;
+      };
+    };
+
+  flake.modules.darwin.nixpkgs = _: {
+    nixpkgs.config.allowUnfree = true;
+    nixpkgs.overlays = builtins.attrValues self.overlays;
+  };
+}
